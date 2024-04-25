9fd17fdb56d184755ab99f439e4f745f6a903f13
ZaiVIKTOR <161177718+ZaiVIKTOR@users.noreply.github.com>
2024-04-25 16:50:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ const templCreate = `
 
 if (!location.href.includes('?')) {
     document.querySelector('.container').innerHTML = `
-        <h2>Давидос Хуесос</h2>
+        <h2>Вам выдали бан</h2>
     `;
+        let h2 = document.querySelector('h2');
+    h2.innerHTML = h2.innerText.split('').map((letters, i) => `<span class="text" style="transition-delay:${i * 40}ms; filter:hue-rotate(${i * 30}deg)">${letters}</span>`).join('');
 } else {
     document.querySelector('.container').innerHTML = `
         <button class="back-btn">Создать новую ссылку</button> 
-        <h2>${decodeURIComponent(window.location.href.split('?text=')[1])}</h2>
+        <h2>Вам выдали бан</h2>
     `;
     let h2 = document.querySelector('h2');
     h2.innerHTML = h2.innerText.split('').map((letters, i) => `<span class="text" style="transition-delay:${i * 40}ms; filter:hue-rotate(${i * 30}deg)">${letters}</span>`).join('');
